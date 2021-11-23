a13b0649a10833918f8f4c2e1cb8f6c80a543cc2
Denis Zickuhr <deniszickuhr@gmail.com>
2021-11-23 19:34:35 -0300

    feat: added support for workbar

--- AgMed/View/src/App.js
+++ AgMed/View/src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useState } from "react";
+import "./App.css";
+import Workbar from "./Components/Workbar/Container";
+import Option from "./Components/Workbar/Option";
 
 function App() {
+  function handleOptionClick(OptionID) {
+    console.log(OptionID);
+
+    options = optionsList
+
+    const newOption = options.map((option) => {
+      if (option.id === OptionID) {
+        return { ...option, id: 4 };
+      } else {
+        return option;
+      }
+    });
+    
+    setOptions(newOption);
+  }
+
+  let [options, setOptions] = useState([
+    {
+      id: "1",
+      texto: "Agenda",
+      estado: false,
+    },
+    {
+      id: "2",
+      texto: "Cadastros",
+      estado: false,
+    },
+    {
+      id: "3",
+      texto: "Horários",
+      estado: false,
+    },
+  ]);
+
+  const optionsList = [
+    {
+      id: "1",
+      texto: "Agenda",
+      estado: false,
+    },
+    {
+      id: "2",
+      texto: "Cadastros",
+      estado: false,
+    },
+    {
+      id: "3",
+      texto: "Horários",
+      estado: false,
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment className="App">
+      <Workbar
+        options={options}
+        handleOptionClick={handleOptionClick}
+      ></Workbar>
+    </Fragment>
   );
 }
 
